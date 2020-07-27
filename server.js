555500053c5cbfa75f331fca74e6a188ec67bce6
okaabe <54871261+ghostakari-s@users.noreply.github.com>
2020-07-26 23:37:32 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const cors = require('cors');
+const ytdl = require('ytdl-core');
+const app = express();
+const path = require('path');
+
+app.use(cors());
+
+
+
+app.get('/download/:format', async (req, res) => {
+  try {
+    if (!["mp3", "mp4"].includes(req.params.format)) return res.json({status: "Failed", message: "Invalid format"});
+    if (!req.query.url) return res.json({status: "Failed", message: "Unknown querys"})
+    if (!ytdl.validateURL(req.query.url)) return res.json({status: "Failed", message: "Invalid url"});
+
+    let downloadOptions = { format: req.params.format.toLowerCase() };
+
+    if (req.params.format.toLowerCase() === "mp4") {
+      Object.assign(downloadOptions, {
+        format: "audioonly"
+      });
+    }
+
+    const videoDetails = await (await ytdl.getBasicInfo(req.query.url)).videoDetails;
+
+    res.header('Content-Disposition', `attachment; filename="${videoDetails.title}.${req.params.format.toLowerCase()}"`);
+    await ytdl(req.query.url, downloadOptions).pipe(res)
+
+  } catch (err) {
+    console.log(err);
+    return res.json({
+      status: "Failed",
+      message: "A error occurred"
+    })
+  }
+})
+
+app.listen(4000, () => {
+  console.log("Listening on port 4000")
+})
